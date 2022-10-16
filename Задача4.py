c301c307c748288b008f8e60c6fe3d55a082dad6
Kostas <langas.kostya@mail.ru>
2022-10-16 14:09:43 +0500

    наименьшее общее кратное

--- Задача4.py
+++ Задача4.py
@@ -2,7 +2,7 @@
 
 a, b = list(map(int, input().split()))
 flag = True
-for i in range(2, min(a, b) + 1):
+for i in range(min(a, b) + 1, 2, -1):
     if a % i == 0 and b % i == 0:
         print(i)
         flag = False
